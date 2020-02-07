a7990fab164c31f882bdaa7d29d4530b4df72631
Kyle McKenna <mckennakyler@gmail.com>
2020-02-07 00:33:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,8 @@ class Artist
     Song.all # has many songs
   end 
    
-  def add_song(song)
-    song.artist = self 
+  def add_song(song) #takes in an argument of a song
+    song.artist = self #associates that song with the artist by telling the song that it belongs to that artist
     @@songs << song 
   end 
   
